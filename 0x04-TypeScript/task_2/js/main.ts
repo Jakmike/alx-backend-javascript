4c9ac0532901640c3f75d5b8576d8965662f29ea
Jakmike <asomughakenenna@gmail.com>
2023-07-05 17:22:58 +0000

    task 0,1 and 2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,27 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+}
+
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  location: string;
+  fullTimeEmployee: boolean;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
