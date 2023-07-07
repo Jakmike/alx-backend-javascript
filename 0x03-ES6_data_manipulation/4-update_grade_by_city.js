2c9a3eeae49ac1308404b717c229c83c87dfdd7a
Jakmike <asomughakenenna@gmail.com>
2023-07-07 22:19:04 +0000

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-para-reassign */
-/* eslint-disable no-prototype-builts */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-prototype-builtins */
 export default function updateStudentGradeByCity(list, city, newGrade) {
   return list
     .filter((obj) => obj.location === city)
